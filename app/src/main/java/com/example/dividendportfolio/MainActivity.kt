c46281164ab73597188a55354e2af1405c8997b9
viveroa2291 <viveroa2291@uwec.edu>
2022-06-16 15:00:35 -0500

    Update
    Added some data to test out the line chart.
    
    - Plan on added more details onto the home page.

--- app/src/main/java/com/example/dividendportfolio/MainActivity.kt
+++ app/src/main/java/com/example/dividendportfolio/MainActivity.kt
@@ -19,19 +19,37 @@ class MainActivity : AppCompatActivity() {
         chart = findViewById(R.id.chart)
 
         val series = LineGraphSeries<DataPoint>()
-
-        val arr = DoubleArray(5) {1.0}
-
-        for (x in 0..90)
+        val series2021 = LineGraphSeries<DataPoint>()
+        val series2022 = LineGraphSeries<DataPoint>()
+        val arr2020 = arrayOf(0.0, 0.0, 0.0, 0.0, 1.29, 1.31, 0.32, 1.9, 2.34, 3.13, 8.09, 3.67)
+        val arr2021 = arrayOf(5.11, 8.09, 5.92, 7.26, 6.97, 8.13, 12.25, 11.42, 12.21, 15.19, 15.32, 16.5)
+        val arr2022 = arrayOf(19.02, 14.36, 16.21, 16.84, 26.46, 14.19, .06, 0.0, 0.0, 0.0, 0.0, 0.0)
+        for (x in 0..11)
         {
-            val y = Math.sin(2*x*.2)-2*Math.sin(x*.2)
-            series.appendData(DataPoint(x.toDouble(), y), true, 90)
+           // val y = Math.sin(2*x*.2)-2*Math.sin(x*.2)
+               val y = arr2020[x]
+            series.appendData(DataPoint(x.toDouble(), y), true, 11)
+        }
+        for (x in 0..11)
+        {
+            val y = arr2021[x]
+            series2021.appendData(DataPoint(x.toDouble(), y), true, 11)
+        }
+        for(x in 0..11)
+        {
+            val y = arr2022[x]
+            series2022.appendData(DataPoint(x.toDouble(), y), true, 11)
         }
         chart.addSeries(series)
+        chart.addSeries(series2021)
+        chart.addSeries(series2022)
         series.setColor(Color.RED)
-        series.setTitle("January 2020")
+        series2021.setColor(Color.BLUE)
+        series2022.setColor(Color.YELLOW)
         chart.setTitle("Dividend Record")
+
+        val gridLabel = chart.gridLabelRenderer
+        gridLabel.setHorizontalAxisTitle("Months")
+        gridLabel.setVerticalAxisTitle("Amount")
     }
-
 }
-
