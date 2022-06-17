95fa8e20dd28cdd9e42f58ed6b4aaadd6cf0d3de
viveroa2291 <viveroa2291@uwec.edu>
2022-06-17 14:44:17 -0500

    Update
    Fixed up all of the constraints issues. I plan to do something with the years.

--- app/src/main/java/com/example/dividendportfolio/MainActivity.kt
+++ app/src/main/java/com/example/dividendportfolio/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         val series2022 = LineGraphSeries<DataPoint>()
         val arr2020 = arrayOf(0.0, 0.0, 0.0, 0.0, 1.29, 1.31, 0.32, 1.9, 2.34, 3.13, 8.09, 3.67)
         val arr2021 = arrayOf(5.11, 8.09, 5.92, 7.26, 6.97, 8.13, 12.25, 11.42, 12.21, 15.19, 15.32, 16.5)
-        val arr2022 = arrayOf(19.02, 14.36, 16.21, 16.84, 26.46, 14.19, .06, 0.0, 0.0, 0.0, 0.0, 0.0)
+        val arr2022 = arrayOf(19.02, 14.36, 16.21, 16.84, 26.46, 14.74, .1, 0.0, 0.0, 0.0, 0.0, 0.0)
         for (x in 0..11)
         {
            // val y = Math.sin(2*x*.2)-2*Math.sin(x*.2)
